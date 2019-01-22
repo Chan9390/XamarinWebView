d3fd3ba2c2dd03bc78e896393f904bb3ce4ab919
Chandrapal Badshah <chandrapal.badshah@aux.go-jek.com>
2019-01-22 23:57:20 +0530

    Initial commit

---
+++ Badshah/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Badshah
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Browser.Source = "https://twitter.com/bnchandrapal";
+        }
+    }
+}
